649812247b2588d21434cfb52607dddecefd1e37
Bruno Dalagnol <dalagnolbruno88@gmail.com>
2024-11-27 00:41:11 -0300

    feat: finish Change Model page

--- app/app.py
+++ app/app.py
@@ -1,16 +1,78 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request, redirect, url_for
 import pandas as pd
 import os
 
 app = Flask(__name__)
+currentModel = 'RandomForestRegressor'
 
 def getCsv():
     os.chdir(os.path.dirname(os.path.realpath(__file__)))
     return pd.read_csv("../datasource/spotify_most_streamed_songs.csv")
 
+@app.route('/fit-model', methods=['POST'])
+def fit_model():
+    global currentModel
+    
+    currentModel = request.form['model']
+
+    print(request.form)
+    return redirect(url_for('change_model'))
+
 @app.route('/change-model', methods=["GET"])
 def change_model():
-    return render_template(template_name_or_list='change_model.html')
+    global currentModel
+
+    data = {
+        'model': "RandomForestRegressor",
+        'params': {
+            'max_depth': 30,
+            'min_samples_leaf': 4,
+            'min_samples_split': 2,
+            'n_estimators': 100,
+        }
+    }
+
+    match request.args.get('model', currentModel):
+        case 'RandomForestRegressor':
+            data = {
+                'model': 'RandomForestRegressor',
+                'params': {
+                    'max_depth': 0,
+                    'min_samples_leaf': 0, 
+                    'min_samples_split': 0, 
+                    'n_estimators': 0
+                }
+            }
+        case 'GradientBoostingRegressor':
+            data = {
+                'model': 'GradientBoostingRegressor',
+                'params': {
+                    'learning_rate': 0.05, 
+                    'max_depth': 3, 
+                    'n_estimators': 100
+                }
+            }
+        case 'LightGBM':
+            data = {
+                'model': 'LightGBM',
+                'params': {
+                    'learning_rate': 0.1, 
+                    'max_depth': 3, 
+                    'n_estimators': 50, 
+                    'num_leaves': 31
+                }
+            }
+        case 'AdaBoostRegressor':
+            data = {
+                'model': 'AdaBoostRegressor',
+                'params': {
+                    'learning_rate': 0.01, 
+                    'n_estimators': 100
+                }
+            }
+
+    return render_template(template_name_or_list='change_model.html', data=data)
+
 
 @app.route('/', methods=["GET"])
 def home():
